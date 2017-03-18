40e5ea172eae3cbae5de52d3baa29a58685c13d2
Gabriel Certeza <gabrielscerteza@gmail.com>
2017-03-18 13:04:07 -0700

    Resolve merge conflict

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -2,7 +2,7 @@ import './css/calendar.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Router, Route } from 'react-router-dom';
-import $ from 'jquery';  
+import $ from 'jquery';
 import Entrance from './components/entrance.jsx';
 import main from './components/main.jsx';
 import login from './components/login.jsx';
@@ -17,18 +17,28 @@ const history = createHistory();
 class App extends React.Component {
   constructor(props) {
     super(props);
-    // this.state = {
-    //   attractionsResults: dummyYelpAttractionData,
-    //   restaurantsResults: dummyYelpRestaurantData,
-    // };
+    this.state = {
+      // attractionsResults: dummyYelpAttractionData,
+      // restaurantsResults: dummyYelpRestaurantData,
+      items: [],
+      location: '',
+    };
   }
 
+  changeLocationFromSearch(locationFromSearch) {
+    this.setState({
+      location: locationFromSearch,
+    });
+  }
   render() {
     return (
       <Router history={history}>
         <div>
-          <Route path="/" component={Entrance} />
-          <Route path="/main" component={main} />
+          <Route
+            path="/" component={() =>
+            (<Entrance changeLocationFromSearch={this.changeLocationFromSearch} />)}
+          />
+          <Route path="/main" component={() => (<main location={this.state.location} />)} />
           <Route path="/login" component={login} />
         </div>
       </Router>
