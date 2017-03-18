82a5766c8c2a74bb791bf9022fae57c67262e661
Gabriel Certeza <gabrielscerteza@gmail.com>
2017-03-18 13:08:27 -0700

    (Refactor) change weather to pass down props with respect to class instantiation

--- client/src/components/main.jsx
+++ client/src/components/main.jsx
@@ -7,24 +7,6 @@ import dummyYelpAttractionData from './../../../dummyYelpAttractionData.js';
 import dummyYelpRestaurantData from './../../../dummyYelpRestaurantData.js';
 import Weather from './weather.jsx';
 
-const Main = (props) => (
-  <div>
-    <header>
-      The main page of Safe Travel
-    </header>
-    <div>
-      <Link to="/">Go back to Entrance</Link>
-    </div>
-    <div>
-      <Link to="/login">login</Link>
-    </div>
-    <Weather location={props.location} />
-    <footer>
-      Be Safe!!
-    </footer>
-  </div>
-);
-
 class Main extends React.Component {
   constructor(props) {
     super(props);
