ae09a68c62315168b133819e3eeef013e5f6454f
Gabriel Certeza <gabrielscerteza@gmail.com>
2017-03-18 13:00:50 -0700

    Remove second Main component and move Weather component

--- client/src/components/main.jsx
+++ client/src/components/main.jsx
@@ -7,27 +7,7 @@ import dummyYelpAttractionData from './../../../dummyYelpAttractionData';
 import dummyYelpRestaurantData from './../../../dummyYelpRestaurantData';
 import Weather from './weather.jsx';
 
-<<<<<<< HEAD
-=======
-const Main = (props) => (
-  <div>
-    <header>
-      The main page of Safe Travel
-    </header>
-    <div>
-      <Link to="/">Go back to Entrance</Link>
-    </div>
-    <div>
-      <Link to="/login">login</Link>
-    </div>
-    <Weather location={props.location} />
-    <footer>
-      Be Safe!!
-    </footer>
-  </div>
-);
 
->>>>>>> Resolve merge conflict
 class Main extends React.Component {
   constructor(props) {
     super(props);
@@ -50,6 +30,7 @@ class Main extends React.Component {
       <div>
         <Link to="/login">login</Link>
       </div>
+      <Weather location={props.location} />
       <footer>
         Be Safe!!
       </footer>
