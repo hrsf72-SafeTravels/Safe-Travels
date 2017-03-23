be1ac4d7717d39a12bbab1ebafc31725a7551660
Gabriel Certeza <gabrielscerteza@gmail.com>
2017-03-22 21:25:01 -0700

    (fix) display dates on date selection for calendar

--- client/src/Index.jsx
+++ client/src/Index.jsx
@@ -34,9 +34,9 @@ class App extends React.Component {
     this.getSavedTrips = this.getSavedTrips.bind(this);
   }
 
-  // shouldComponentUpdate() {
-  //   return false;
-  // }
+  shouldComponentUpdate() {
+    return false;
+  }
 
   getSavedTrips() {
     Axios.get('/savedTrips')
